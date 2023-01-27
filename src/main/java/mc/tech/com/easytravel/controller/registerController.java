8e705189d55bb27586d630b7688d0824b974fef5
draybulambo <draybulambo45@gmail.com>
2023-01-27 22:35:45 +0200

    design registration page

--- src/main/java/mc/tech/com/easytravel/controller/registerController.java
+++ src/main/java/mc/tech/com/easytravel/controller/registerController.java
@@ -1,2 +1,20 @@
-package mc.tech.com.easytravel.controller;public class registerController {
+package mc.tech.com.easytravel.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+@Slf4j
+@RestController
+@Controller
+public class registerController {
+
+    @GetMapping({"/register"})
+    public ModelAndView register()
+    {
+        ModelAndView modelAndView = new ModelAndView("register");
+        modelAndView.addObject("passing register form", "login");
+        return modelAndView;
+    }
 }
